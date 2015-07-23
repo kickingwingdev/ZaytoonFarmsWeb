260ad1c03825bffd955031fe99bd4b05ad333a21
kickingwingdev <shaun_theron@live.co.za>
2015-07-23 20:11:25 +0200

    Add _header

---
+++ resources/views/_header.blade.php
@@ -0,0 +1,34 @@
+<div class="header-top-bar-container clearfix">
+    <div class="header-top-bar">
+        <ul class="contact-details clearfix">
+            <li class="template-phone">
+                013 7127911/2
+            </li>
+            <li class="template-mail">
+                zaytoonpoultry@telkomsa.net
+            </li>
+            <li class="template-clock">
+                Mon - Fri: 08.00 - 17.00
+            </li>
+        </ul>
+        <div class="search-container">
+            <a class="template-search" href="#" title="Search"></a>
+            <form class="search" action="search.html">
+                <input type="text" name="s" placeholder="Search..." value="Search..." class="search-input hint">
+                <fieldset class="search-submit-container">
+                    <span class="template-search"></span>
+                    <input type="submit" class="search-submit" value="">
+                </fieldset>
+            </form>
+        </div>
+        <ul class="social-icons">
+            <li>
+                <a target="_blank" href="#" class="social-facebook" title="facebook"></a>
+            </li>
+            <li>
+                <a target="_blank" href="#" class="social-twitter" title="twitter"></a>
+            </li>
+        </ul>
+    </div>
+    <a href="#" class="header-toggle template-arrow-up"></a>
+</div>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Zaytoon Farms</title>
+    <!--Fonts-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:100,300,400,500,600,700,900">
+    <link rel="stylesheet" type="text/css" href="fonts/streamline-small/styles.css">
+    <link rel="stylesheet" type="text/css" href="fonts/streamline-large/styles.css">
+    <link rel="stylesheet" type="text/css" href="fonts/template/styles.css">
+    <link rel="stylesheet" type="text/css" href="fonts/social/styles.css">
+    <!--Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-qtip.css">
+</head>
+
+<body>
+    @yield('content')
+    <!--Scripts-->
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+    <!--Scripts-->
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="js/jquery.ba-bbq.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.11.4.custom.min.js"></script>
+    <script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery.carouFredSel-6.2.1-packed.js"></script>
+    <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
+    <script type="text/javascript" src="js/jquery.transit.min.js"></script>
+    <script type="text/javascript" src="js/jquery.hint.min.js"></script>
+    <script type="text/javascript" src="js/jquery.costCalculator.min.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax.min.js"></script>
+    <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js/jquery.qtip.min.js"></script>
+    <script type="text/javascript" src="js/jquery.blockUI.min.js"></script>
+    <script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/odometer.min.js"></script>
+</body>
+
+</html>
