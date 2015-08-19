7e699e6916ba794685a76fd666e1ed3c563f93c9
kickingwingdev <shaun_theron@live.co.za>
2015-08-19 12:18:21 +0200

    Tweak service images for the second run

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -12,42 +12,42 @@
 	    <ul class="services-list clearfix padding-top-70">
 	        <li>
 	            <a href="/food-safety" title="Food Safety">
-	                <img src="images/samples/780x520/service-food-safety-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-food-safety-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/food-safety" title="Food Safety">FOOD SAFETY</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, soluta!</p
 	        </li>
 	        <li>
 	            <a href="/halaal" title="Halaal">
-	                <img src="images/samples/780x520/service-halaal-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-halaal-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/halaal" title="Halaal">HALAAL</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, explicabo.</p
 	        </li>
 	        <li>
 	            <a href="/dispatch-and-transport" title="Dispatch and Transport">
-	                <img src="images/samples/780x520/service-dispatch-transport-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-dispatch-transport-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/dispatch-and-transport" title="Dispatch and Transport">DISPATCH AND TRANSPORT</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, commodi!</p
 	        </li>
 	        <li>
 	            <a href="/quality-control" title="Quality Control">
-	                <img src="images/samples/780x520/service-quality-control-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-quality-control-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/quality-control" title="Quality Control">QUALITY CONTROL</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, itaque.</p>
 	        </li>
 	        <li>
 	            <a href="/packaging" title="Packaging">
-	                <img src="images/samples/780x520/service-packaging-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-packaging-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/packaging" title="Packaging">PACKAGING</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, cum.</p>
 	        </li>
 	        <li>
 	            <a href="/custom-orders" title="Custom Orders">
-	                <img src="images/samples/780x520/service-custom-orders-md.jpg" alt="">
+	                <img src="images/samples/780x520/service-custom-orders-md-01.jpg" alt="">
 	            </a>
 	            <h4 class="box-header"><a href="/custom-orders" title="Custom Orders">CUSTOM ORDERS</a></h4>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, facere.</p>
